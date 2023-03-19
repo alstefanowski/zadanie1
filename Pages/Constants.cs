d0cb81fb7c7f1e756b2f7a57b272c4c555d66b4b
alstefanowski <ciuniekk@gmail.com>
2023-03-19 15:09:49 +0100

    Zadanie 2

---
+++ Pages/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.NetworkInformation;
+
+namespace FizzBuzzWeb.Pages
+{
+    public class Constants
+    {
+        public static readonly string Fizz = "Fizz";
+        public static readonly string Buzz = "Buzz";
+        public static readonly string FizzBuzz = "FizzBuzz";
+        public static readonly string Other = "Other";
+    }
+}
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,20 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FizzBuzzWeb.Forms;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
 
 namespace FizzBuzzWeb.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-
+        [BindProperty]
+        public FizzBuzzForm FizzBuzz { get; set; }
+        //[BindProperty(SupportsGet = true)]
+        public string Name;
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
-
-        public void OnGet()
+        public IActionResult OnPost()
         {
-
+            if (FizzBuzz.Number % 3 == 0 && FizzBuzz.Number % 5 != 0 && FizzBuzz.Number > 0)
+                TempData[Constants.Fizz] = "Fizz";
+            if (FizzBuzz.Number % 5 == 0 && FizzBuzz.Number % 3 != 0 && FizzBuzz.Number > 0)
+                TempData[Constants.Buzz] = "Buzz";
+            if (FizzBuzz.Number % 3 == 0 && FizzBuzz.Number % 5 == 0 && FizzBuzz.Number > 0)
+                TempData[Constants.FizzBuzz] = "FizzBuzz";
+            if(FizzBuzz.Number % 3 != 0 && FizzBuzz.Number % 5 != 0 && FizzBuzz.Number > 0)
+                TempData[Constants.Other] = Convert.ToInt32(FizzBuzz.Number) + " nie spełnia kryteriów FizzBuzz w pozostałych przypadkach";
+            return Page();
         }
     }
 }
